0b5d94695767e8b7856a4c1b18583f499a67480d
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-08-18 17:11:39 +0000

    Initial commit

---
+++ APIpokemons.py
@@ -0,0 +1,83 @@
+import requests
+import json
+
+"""
+    Utilizando a API POKEDEX para criação de um arquivo JSON
+    com dados sobre os pokemons
+     
+     dados a serem coletados:
+          id = identificação do pokemon na API
+          name = nome do pokemon
+          types = o tipo do pokemon
+          ability = habilidades que o pokemon possui
+          stats = atributos do pokemon 
+"""
+TOTAL_POKEMONS = 151 #constante com a definição de quantos pokemons serão coletados
+
+def req_api():
+  """ 
+  Requisição da API 
+  e estruturação dos Dados coletados 
+  """
+    
+  bichos = [] 
+  
+  #constroi a url com o parâmetro que será passado
+  url = 'https://pokeapi.co/api/v2/pokemon/'
+     
+    
+  #verifica a resposta da url e retorna o arquivo json da API
+  try:
+    for i in range(TOTAL_POKEMONS): #laço dos pokemons que serão utilizados
+      resp = requests.get(f'{url}{i+1}')
+      if resp.status_code == 200:
+        conteudo = resp.json()
+       
+        #estrutura de dados que será salvo no arquivo JSON
+        a_bichos = {
+          'id': conteudo['id'],
+          'name': conteudo['name'],
+          'sprite': f'{i+1}.png',
+          'types': [],
+          'ability': [],
+          'stats': []
+        }
+        #laços que farão a coleta dos dados a serem salvos
+        for type in conteudo['types']:
+          a_bichos['types'].append(type['type']['name'])
+        for skill in conteudo['abilities']:
+          a_bichos['ability'].append(skill['ability']['name'])
+        for stats in conteudo['stats']:
+          a_bichos['stats'].append({stats['stat']['name']: stats['base_stat']})
+        
+        bichos.append(a_bichos)
+      else:
+        return resp.status_code
+        
+  except:
+    print("Erro na Conexao") #caso a resposta http seja diferente do code 200 retorna o erro
+  
+  return bichos
+
+#função para baixar as imagens dos pokemons
+
+def downloadImages():
+  
+  for i in range(TOTAL_POKEMONS):
+      url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{i+1}.png'
+      resp = requests.get(url, allow_redirects=True)
+      open(f'sprites/{i+1}.png', 'wb').write(resp.content)
+      print(f'Downloaded sprite {i+1}')
+
+#função para salvar a estrutura de dados em arquivo JSON
+def write_to_json(pokemon_lista):
+  with open('listapokemon.json', 'w') as arq:
+    json.dump(pokemon_lista, arq, indent=4, ensure_ascii=False)
+    arq.close()
+
+def main():
+  pokemon = req_api()
+  write_to_json(pokemon)
+  downloadImages()
+if __name__ == '__main__':
+  main()
---
+++ main.py
@@ -0,0 +1,98 @@
+from tkinter import *
+import json
+from PIL import ImageTk, Image
+
+class App():
+
+  def __init__(self, master=None, child=None):
+    """inicializa o App"""
+
+    self.pokemon = []
+    
+    self.fonte = ("Verdana", "10")
+    
+    self.master = Frame(master)
+    self.master['padx'] = 20
+    self.master['pady'] = 15
+    self.master.pack()
+
+ 
+    self.child = Frame(child)
+    self.child['padx'] = 5
+    self.child['pady'] = 5
+    self.child.pack()
+  
+  #função do layout para pesquisa do pokemon
+  def l_pesquisa(self):
+    """ Tela Principal com a opção de busca"""
+  
+    self.title = Label(self.master, text='Pokesim, Pokenão, Pokesim, Pokemon')
+    self.title['pady'] = 5
+    self.title['font'] = ('Calibre', '10', 'bold')
+    self.title.pack()
+
+    self.lblnome = Label(self.child, text='Nome do Pokemon:', font=self.fonte, width=15)
+    self.lblnome.pack()
+    self.txtnome = Entry(self.child)
+    self.txtnome['width'] = 20
+    self.txtnome['font'] = self.fonte
+    self.txtnome.pack()
+
+    self.btnBuscar = Button(self.child, text='Buscar', font=self.fonte, width=5)
+    self.btnBuscar['command'] = self.exibir_pokemon
+    self.btnBuscar.pack()
+ 
+  #função com a estrutura do perfil do pokemon sendo mostrado em um janela pop-up
+  def exibir_pokemon(self):
+    """ Tela onde será mostrado o Pokemon pesquisado """
+    
+    top = Toplevel()
+    top.title('Eu Escolho Você')
+    top.geometry('350x300')
+
+    n = self.txtnome.get()
+    self.pokemon = n
+    self.txtnome.delete(0, END)
+
+    with open('listapokemon.json', 'r') as arq:
+      pokelist = json.load(arq)
+      for poke in pokelist:
+        if self.pokemon in poke['name']:
+          label0 = Label(top, text=poke['name'].title(), fg='red')
+          label0.pack()
+          label2 = Label(top, text=poke['types'])
+          label2.pack()
+          label3 = Label(top, text=poke['ability'])
+          label3.pack()
+          for skill in poke['stats']:
+            for key, value in skill.items():
+              label4 = Label(top, text=key+' - '+str(value))
+              label4.pack()
+          img = ImageTk.PhotoImage(Image.open(f"./sprites/{poke['sprite']}"))
+          label5 = Label(top, image=img)
+          label5.pack()
+          
+
+root = Tk()
+app = App(root)
+root.title("Pokedex Marota")
+root.geometry('450x290')
+
+##### Cria o menu de navegação do Sistema #####
+
+menu = Menu(root)
+root.config(menu=menu)
+
+filemenu = Menu(menu, tearoff=0)
+menu.add_cascade(label='File', menu=filemenu)
+filemenu.add_command(label='Buscar Pokemon', command=app.l_pesquisa())
+
+filemenu.add_separator()
+filemenu.add_command(label='Sair', command=root.quit)
+
+helpmenu = Menu(menu, tearoff=0)
+menu.add_cascade(label='Help', menu=helpmenu)
+helpmenu.add_command(label='About')
+
+
+root.mainloop()
